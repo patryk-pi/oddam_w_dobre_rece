79d72655db5f774000531758c3727ecfa74507a1
patryk-pi <patryk.piasecki1@gmail.com>
2023-09-02 21:44:48 +0200

    added missing import in navigation element

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -1,11 +1,11 @@
-const Navigation = () => {
+import React from "react";
 
+const Navigation = () => {
     return (
         <div>
             <nav>Nawigacja</nav>
         </div>
-    )
-
-}
+    );
+};
 
 export default Navigation;
