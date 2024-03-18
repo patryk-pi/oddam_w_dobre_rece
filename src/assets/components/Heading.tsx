e185592c18c72c76d9fed6068676bfb40c700372
patryk-pi <patryk.piasecki1@gmail.com>
2024-03-18 16:35:52 +0100

    done styling for the whole header

--- src/assets/components/Heading.tsx
+++ src/assets/components/Heading.tsx
@@ -1,16 +1,28 @@
-import { StyledHeading } from "./styles/Heading.styled";
+import {
+    StyledHeading,
+    StyledHeadingButtons,
+    StyledHeadingContainer,
+} from "./styles/Heading.styled";
 
 const Heading = () => {
     return (
         <StyledHeading>
-            <div>
+            <StyledHeadingContainer>
                 <h1>Zacznij pomagać!</h1>
-                <h1>Oddaj niechciane rzeczy w zaufane ręgfgce</h1>
+                <h1>Oddaj niechciane rzeczy w zaufane ręce</h1>
                 <img
                     src="src/assets/images/Decoration@2x.png"
                     alt="Decoration"
                 />
-            </div>
+                <StyledHeadingButtons>
+                    {/* ROUTES TO BE DEFINED!!! */}
+                    <a href="#">
+                        Oddaj
+                        <br /> rzeczy
+                    </a>
+                    <a href="#">Zorganizuj zbiórkę</a>
+                </StyledHeadingButtons>
+            </StyledHeadingContainer>
         </StyledHeading>
     );
 };
--- src/assets/components/styles/Heading.styled.ts
+++ src/assets/components/styles/Heading.styled.ts
@@ -4,21 +4,48 @@ export const StyledHeading = styled.div`
     margin-left: -18rem;
 
     div {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        margin-top: 22rem;
-        align-items: center;
-    }
-
-    h1 {
-        font-size: 3.8rem;
-        font-weight: 300;
-        text-align: center;
     }
 
     img {
         height: 3.3rem;
         width: 25.3rem;
+        margin-bottom: 6.5rem;
+    }
+`;
+
+export const StyledHeadingContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 22rem;
+    align-items: center;
+
+    h1 {
+        font-size: 3.8rem;
+        font-weight: 300;
+        text-align: center;
+
+        &:last-of-type {
+            margin-bottom: 2.5rem;
+        }
+    }
+`;
+
+export const StyledHeadingButtons = styled.div`
+    display: flex;
+    gap: 9.5rem;
+
+    a {
+        /* display: inline-block; */
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        align-items: center;
+        text-transform: uppercase;
+        font-size: 3.6rem;
+        font-weight: 300;
+        width: 31rem;
+        height: 12rem;
+        border: 1px solid ${({ theme }) => theme.colors.black};
     }
 `;
