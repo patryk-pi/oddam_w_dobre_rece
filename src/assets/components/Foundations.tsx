dff585232a826e47f966c386b6a77493c8d93b64
patryk-pi <patryk.piasecki1@gmail.com>
2024-03-21 16:48:47 +0100

    added and styled OrgList and OrgListItems

--- src/assets/components/Foundations.tsx
+++ src/assets/components/Foundations.tsx
@@ -1,15 +1,46 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Decoration from "./Decoration";
-import { StyledFoundations } from "./styles/Foundations.styled";
+import {
+    StyledFoundations,
+    StyledOrgList,
+    StyledOrgListItem,
+} from "./styles/Foundations.styled";
 
-const [organization, setOrganization] = useState<string>("foundation");
+interface OrgItemProps {
+    active?: boolean;
+    onClick: () => void;
+}
 
 const Foundations = () => {
+    const [organization, setOrganization] = useState<string>("foundation");
+    console.log(organization);
+
     return (
         <StyledFoundations>
             <h2>Komu pomagamy</h2>
             <Decoration />
+            <StyledOrgList>
+                <StyledOrgListItem
+                    active={organization === "foundation"}
+                    onClick={() => setOrganization("foundation")}
+                >
+                    <h4>Fundacjom</h4>
+                </StyledOrgListItem>
+                <StyledOrgListItem
+                    active={organization === "ngo"}
+                    onClick={() => setOrganization("ngo")}
+                >
+                    <h4>Organizacjom pozarządowym</h4>
+                </StyledOrgListItem>
+                <StyledOrgListItem
+                    active={organization === "local"}
+                    onClick={() => setOrganization("local")}
+                >
+                    <h4>Lokalnym zbiórkom</h4>
+                </StyledOrgListItem>
+            </StyledOrgList>
         </StyledFoundations>
     );
 };
+
 export default Foundations;
--- src/assets/components/styles/Foundations.styled.ts
+++ src/assets/components/styles/Foundations.styled.ts
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+interface OrgItemProps {
+    active?: boolean;
+    onClick: () => void;
+}
+
 export const StyledFoundations = styled.section`
     display: flex;
     flex-direction: column;
@@ -8,6 +13,29 @@ export const StyledFoundations = styled.section`
 
     h2 {
         font-size: 3.8rem;
+        font-weight: 400;
+    }
+`;
+
+export const StyledOrgList = styled.ul`
+    display: flex;
+    gap: 10rem;
+    padding: 0;
+`;
+
+export const StyledOrgListItem = styled.li<OrgItemProps>`
+    width: 21rem;
+    height: 8rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid
+        ${({ active, theme }) => (active ? theme.colors.black : "transparent")};
+    text-align: center;
+    cursor: pointer;
+
+    h4 {
+        font-size: 2.4rem;
         font-weight: 400;
     }
 `;
