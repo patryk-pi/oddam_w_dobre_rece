deb1d229aa12c3f4766cde7452f4ce41d5dff7b0
patryk-pi <patryk.piasecki1@gmail.com>
2024-04-01 09:56:23 +0200

    implemented orgsRoutes

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db";
+import orgsRoutes from "./routes/orgsRoutes";
 
 connectDB();
 
@@ -16,3 +17,5 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
+
+app.use("/api/orgs", orgsRoutes);
