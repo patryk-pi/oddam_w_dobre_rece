e9a86e1857ae51f617aaff64063116d9f6b824ae
patryk-pi <patryk.piasecki1@gmail.com>
2024-04-08 13:57:08 +0200

    changes in orgsController, added limit and cursor for pagination

--- backend/controllers/orgsController.ts
+++ backend/controllers/orgsController.ts
@@ -1,8 +1,16 @@
 import asyncHandler from "../middleware/asyncHandler";
 import Org from "../models/orgModel";
+import { Request, Response } from "express";
 
-const getOrgs = asyncHandler(async (req, res) => {
-    const orgs = await Org.find({});
+const getOrgs = asyncHandler(async (req: Request, res: Response) => {
+    const limit = 3; // Number of results per page
+
+    let cursor = req.query.cursor; // Get the cursor from query parameter
+    if (!cursor) {
+        cursor = ""; // Set default cursor if not provided
+    }
+
+    const orgs = await Org.find({ _id: { $gt: cursor } }).limit(limit); // Query for organizations with pagination using cursor
     res.json(orgs);
 });
 
