2e5a732035bee9c33ae096f0681b1955d6737b39
patryk-pi <patryk.piasecki1@gmail.com>
2024-04-20 14:18:34 +0200

    changed styled of LoginForm

--- frontend/src/assets/components/styles/LoginForm.styled.ts
+++ frontend/src/assets/components/styles/LoginForm.styled.ts
@@ -1,13 +1,22 @@
 import styled from "styled-components";
 
 export const StyledLoginForm = styled.section`
-    margin: 0 auto;
+    margin: 6.7rem auto 0;
+    width: 39rem;
 
     form {
         background-color: ${({ theme }) => theme.colors.white};
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 4rem 6.8rem;
 
         input {
             background-color: transparent;
+        }
+
+        label {
+            font-size: 1.6rem;
         }
     }
 `;
