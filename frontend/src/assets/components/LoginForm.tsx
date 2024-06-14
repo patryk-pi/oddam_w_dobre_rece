d2a49bfe08ce4a30642199cbe693e76371265a17
patryk-pi <patryk.piasecki1@gmail.com>
2024-06-14 13:40:03 +0200

    added buttons to login form

--- frontend/src/assets/components/LoginForm.tsx
+++ frontend/src/assets/components/LoginForm.tsx
@@ -1,17 +1,26 @@
-import { StyledLoginForm } from "./styles/LoginForm.styled";
+import {
+    StyledActionButtons,
+    StyledLoginForm,
+} from "./styles/LoginForm.styled";
 
 const LoginForm = () => {
     return (
         <StyledLoginForm>
             <form>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label htmlFor="password">Hasło</label>
-                    <input type="password" />
-                </div>
+                <main>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Hasło</label>
+                        <input type="password" />
+                    </div>
+                </main>
+                <StyledActionButtons>
+                    <a href="/signup">Zarejestruj się</a>
+                    <button type="submit">Zaloguj się</button>
+                </StyledActionButtons>
             </form>
         </StyledLoginForm>
     );
